14c82a6430b8f9778b0f81ace08eb280a51e8032
Mykola Yefremov <efremovnikolaus@gmail.com>
2026-06-03 12:10:03 +0200

    docs(openapi): add local API metadata

--- src/main/java/com/emt/configuration/OpenApiConfiguration.java
+++ src/main/java/com/emt/configuration/OpenApiConfiguration.java
@@ -1,9 +1,17 @@
 package com.emt.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(servers = @Server(url = "http://localhost:8080", description = "Local server"))
+@OpenAPIDefinition(
+    info =
+        @Info(
+            title = "Elo Match Tracker",
+            version = "0.0.1",
+            description =
+                "Server-rendered Elo ranking application. REST endpoints can be added later on top of the existing service layer."),
+    servers = @Server(url = "http://localhost:8080", description = "Local server"))
 public class OpenApiConfiguration {}
